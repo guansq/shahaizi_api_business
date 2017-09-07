52a39192a6edaae13b392133d03f8bfac9dd0062
GuanShaoQiu <GuanShaoQiu@ruitukeji.com>
2017-09-07 17:03:19 +0800

    fix bug

--- application/api/controller/User.php
+++ application/api/controller/User.php
@@ -332,10 +332,10 @@ class User extends Base {
         //是否开启注册验证码机制
         if(check_mobile($username)){
             //校验验证码
-            /*$result = MsgService::verifyCaptcha($username,'reg',$code);
+            $result = MsgService::verifyCaptcha($username,'reg',$code);
             if($result['status'] != 1){
                 returnJson(-1,'验证码输入有误');
-            }*/
+            }
             //$res = $this->userLogic->check_validate_code($code, $username  , $type , $session_id , $scene);
             //if($res['status'] != 1) exit(json_encode($res));
         }
